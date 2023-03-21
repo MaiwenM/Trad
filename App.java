c866c621a4ffc04e78dd0dd19ea2ee7b79b2615b
Maïwen Menez <121941644+MaiwenM@users.noreply.github.com>
2023-03-21 16:50:05 +0100

    Ajout boucle while pour re-demander le choix utilisateur

--- App.java
+++ App.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner scan = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int choix = 1;
 
         Anglais vAnglais = new Anglais();
@@ -14,7 +14,12 @@ public class App {
         System.out.println("1 : Anglais");
         System.out.println("2 : Breton");
 
-        choix = scan.nextInt();
+        choix = sc.nextInt();
+
+        while((choix > 2) || (choix <= 0) ){
+            System.out.println("Valeur invalide, merci de faire un choix entre 1 (Anglais) et 2 (Breton)");
+            choix = sc.nextInt();
+        }
 
         if(choix == 1){
             System.out.println(vAnglais.getTraduction("Bonjour"));
